8f90e775d6b02daac5ff8b412ed704c194ebe365
Mustafa <codingbox@outlook.com>
2021-08-12 20:15:48 -0400

    init

---
+++ src/test/java/ca/shubbar/dependencyinjection/DependencyInjectionApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.shubbar.dependencyinjection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DependencyInjectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
